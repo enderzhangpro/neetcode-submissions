8d2482d62408f6c2b5214fe04c758d6908778598
David Liu <41446535+enderzhangpro@users.noreply.github.com>
2026-05-07 11:33:09 -0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.py
@@ -0,0 +1,37 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.array = []
+
+
+    def get(self, i: int) -> int:
+        return self.array[i]
+
+
+    def set(self, i: int, n: int) -> None:
+        self.array[i] = n
+
+
+    def pushback(self, n: int) -> None:
+        if len(self.array) == self.capacity:
+            self.resize()
+        self.array.append(n)
+
+
+    def popback(self) -> int:
+        result = self.array[-1]
+        del self.array[-1]
+        return result
+ 
+
+    def resize(self) -> None:
+        self.capacity *= 2
+
+
+    def getSize(self) -> int:
+        return len(self.array)
+        
+    
+    def getCapacity(self) -> int:
+        return self.capacity
